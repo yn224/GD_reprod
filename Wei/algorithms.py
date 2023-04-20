15d7f0a3466958b0c79757c45811de9bbc9f19c4
Wei <125891913+TheOnlyWei24@users.noreply.github.com>
2023-04-19 23:09:29 -0400

    Update algorithms.py

--- Wei/algorithms.py
+++ Wei/algorithms.py
@@ -180,6 +180,55 @@ def Logistic_Regression_SAG(x, y, eta, K, L, q=None):
         
     return w, b, np.array(costs)
 
+def Logistic_Regression_SAG_FAST(x, y, eta, K, L, q=None):
+    #Initialize weights and bias
+    b = 0.1
+    w = np.zeros([x.shape[1],1])
+    G = np.zeros_like(w) #Gradient table sum
+    g = np.zeros((x.shape[0], x.shape[1],1)) #Gradient table
+    idxs = []
+    m = 0
+    
+    costs = []
+    y = y.reshape((len(y),1))
+    
+    #For each iteration
+    for k in tqdm(range(K), disable=tqdmSwitch):
+        
+        #Draw random sample with replacement
+        idx = np.random.randint(0,len(y))
+        xx = x[idx]
+        yy = y[idx]
+        
+        #Fixed learning rate
+        a = eta
+        
+        #Make prediction
+        y_pred = bound(sigmoid(xx@w+b))
+
+        #Check if data point has been seen
+        if idx not in idxs:
+            idxs += [idx]
+            m += 1
+        
+        #Calculate current gradient
+        grad = (xx*(y_pred-yy)).reshape((x.shape[1],1))
+        G = G + (grad) - ((g[idx])) 
+        g[idx] = grad
+        
+        #Update weights
+        w = w - a*(G/m + L*w)
+        b = b - a*(y_pred-yy)
+
+        #Compute cost
+        pred = bound(sigmoid(x@w+b))
+        costs += [log_loss(y, pred, labels = [0,1])]
+        
+    if q != None:
+        q.put([w, b, np.array(costs)])
+        
+    return w, b, np.array(costs)
+
 def Logistic_Regression_SAGA(x, y, eta, K, L, q=None):
     #Initialize weights and bias
     b = 0.1
@@ -224,6 +273,69 @@ def Logistic_Regression_SAGA(x, y, eta, K, L, q=None):
         pred = bound(sigmoid(x@w+b))
         costs += [log_loss(y, pred, labels = [0,1])]
         
+    if q != None:
+        q.put([w, b, np.array(costs)])
+        
+    return w, b, np.array(costs)
+
+def Logistic_Regression_finito(x, y, mu, K, q=None):
+    # Initialize weights and bias
+    # b = 0.1
+    w = np.zeros([x.shape[1],1])
+    P = np.zeros((x.shape[0], x.shape[1], 1)) # Weight table
+    G = np.zeros((x.shape[0], x.shape[1], 1)) # Gradient table
+    grad = np.zeros_like(w)
+    idxs = []
+    m = 0
+    b = 0
+    
+    costs = []
+    y = y.reshape((len(y),1))
+    
+    # TODO: you can change this to whatever alpha value it is
+    # - pg. 7 of Finito paper.
+    alpha = 1
+
+    #For each iteration
+    for k in tqdm(range(K), disable=tqdmSwitch):        
+        # Learning rate - mu is convexity constant
+        #a = 1/(alpha * mu * (k+1))
+        a = mu
+
+        # Draw random sample with replacement
+        idx = np.random.randint(0,len(x))
+        xx = x[idx]
+        yy = y[idx]
+
+        # Check if data point has been seen
+        if idx not in idxs:
+            idxs += [idx]
+            m += 1
+        
+        # Take the average of the weights \phi
+        phi_bar = np.sum(P, axis=0) / m
+
+        # Take the average of the gradients
+        g_bar = np.sum(G, axis=0) / m
+
+        # Update weight
+        w = phi_bar - a * g_bar
+        #b = b - a * (y_pred-yy)
+        
+        # Store data
+        P[idx] = w
+        G[idx] = grad
+        
+        # Make prediction
+        y_pred = bound(sigmoid(xx@w+b))
+
+        # Calculate current gradient
+        grad = (xx*(y_pred-yy)).reshape((x.shape[1],1))
+
+        #Compute cost
+        pred = bound(sigmoid(x@w+b))
+        costs += [log_loss(y, pred, labels = [0,1])]
+
     if q != None:
         q.put([w, b, np.array(costs)])
         
